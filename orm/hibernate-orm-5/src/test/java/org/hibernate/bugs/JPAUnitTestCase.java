f4652b32ef4bdb421b65899eff875538ac9ff799
hwirths <hwirths@nde.ag>
2023-07-20 18:21:26 +0200

    Revert "HHH-16961 Implemented the test case." from branch 'main'
    This reverts commit fbeb06970616e16afb2386d0997c7e0aa0431eef.

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,8 +1,5 @@
 package org.hibernate.bugs;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -31,48 +28,11 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh16961Test() throws Exception {
+	public void hhh123Test() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-
-		/*
-		 * Step one : Create an entry for both the port and the device
-		 * table. The device entry gets associated to the port entry.
-		 */
-		Coordinate coordinate = new Coordinate();
-		Port port = new Port();
-		Device device = new Device();
-
-		coordinate.setSlot_number(1);
-		coordinate.setPort_number(1);
-		port.setCoordinate(coordinate);
-		port.setConnection_id(1);
-		
-		entityManager.persist(port);
-		
-		device.setPort(port);
-		
-		entityManager.persist(device);
-		
-		entityManager.getTransaction().commit();
-		entityManager.clear();
-		
-		/*
-		 * Step two : Request the first entry from the device table.
-		 */
-		entityManager.getTransaction().begin();
-		
-		device = entityManager
-				.createQuery("select d from Device d", Device.class)
-				.setMaxResults(1)
-				.getSingleResult();
-		
-		assertNotNull(device);
-		assertNotNull(device.getPort());
-		assertEquals(device.getPort().getConnection_id(), 1);
-		
+		// Do stuff...
 		entityManager.getTransaction().commit();
 		entityManager.close();
 	}
-	
 }
